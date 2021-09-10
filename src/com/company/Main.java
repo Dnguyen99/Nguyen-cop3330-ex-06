be1507cb95be66fe5dd978e5a8ee205996dbe1e4
Dnguyen99 <dylannguyen@Knights.ucf.edu>
2021-09-10 11:56:44 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+
+        System.out.println("What is your current age? ");
+        int currentAge = scan.nextInt();
+        System.out.println("At what age would you like to retire? ");
+        int retireAge = scan.nextInt();
+
+        int yearsLeft = retireAge - currentAge;
+        System.out.println("You have " +yearsLeft+ " years left until you can retire.");
+
+        int retireYear = currentYear + yearsLeft;
+        System.out.println("It's " +currentYear+ ", so you can retire in " +retireYear+ ".");
+    }
+}
