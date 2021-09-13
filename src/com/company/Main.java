7a1005f79cbff1352401e8f1d136026283997895
Dnguyen99 <89670208+Dnguyen99@users.noreply.github.com>
2021-09-13 13:00:45 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,12 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Dylan Nguyen
+ */
+
 package com.company;
 import java.util.*;
 import java.lang.*;
+
 public class Main {
 
     public static void main(String[] args) {
